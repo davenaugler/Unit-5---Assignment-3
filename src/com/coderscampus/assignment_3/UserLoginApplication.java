70260a068d99f134620cbd3adcf6baabaf8973b8
Dave Naugler <davenaugler@gmail.com>
2023-06-05 15:49:23 -0600

    swiching to feature branch

--- src/com/coderscampus/assignment_3/UserLoginApplication.java
+++ src/com/coderscampus/assignment_3/UserLoginApplication.java
@@ -1,7 +1,7 @@
 package com.coderscampus.assignment_3;
 
 import java.io.IOException;
-
+// switching to feature branch to refactor this project
 public class UserLoginApplication {
 
 	public static void main(String[] args) throws IOException {
